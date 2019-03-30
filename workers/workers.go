8bd22e6414f06d7bbfe9b56bc87a548101309775
krispo95 <pogosyan.chris@gmail.com>
2019-03-30 13:12:01 +0300

    sequential calculation

--- workers/workers.go
+++ workers/workers.go
@@ -1,5 +1,12 @@
 package workers
 
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+)
+
 type worker struct {
 	tasks <-chan *TaskTree
 	stop  <-chan struct{}
@@ -14,32 +21,77 @@ type TaskResult struct {
 	Sum    int64 `json:"sum"`
 }
 
-func (w *worker) run() {
-	for {
-		select {
-		case <-w.stop:
-			return
-		case task := <-w.tasks:
-			w.doWork(task)
-		}
+func readJSON(r io.Reader) *TaskTree {
+	tree := new(TaskTree)
+	err := json.NewDecoder(r).Decode(tree)
+	if err != nil {
+		panic("Ошибка преобразования")
 	}
+	return tree
 }
 
-func initWorker() {
-	for i := uint(0); i < 3; i++ {
-		w := &worker{
-			tasks: pool.tasks,
-			stop:  pool.stop,
-			pool:  pool,
-		}
-		go w.run()
-	}
-}
+//
+//func (w *worker) run() {
+//	for {
+//		select {
+//		case <-w.stop:
+//			return
+//		case task := <-w.tasks:
+//			w.doWork(task)
+//		}
+//	}
+//}
 
-func (w *worker) doWork(task *TaskTree) (result TaskResult) {
+//func initWorker() {
+//	for i := uint(0); i < 3; i++ {
+//		w := &worker{
+//			tasks: pool.tasks,
+//			stop:  pool.stop,
+//			pool:  pool,
+//		}
+//		go w.run()
+//	}
+//}
+
+//func (w *worker) doWork(task *TaskTree) (result TaskResult) {
+//	for _, item := range task.Task {
+//		result.Sum += item
+//	}
+//	result.TaskId = task.TaskId
+//	return result
+//}
+
+func doWork(task *TaskTree) (result TaskResult) {
 	for _, item := range task.Task {
 		result.Sum += item
 	}
 	result.TaskId = task.TaskId
 	return result
+}
+
+func seqWork(tasks *TaskTree, parRes int64) (result []TaskResult) {
+
+	res := doWork(tasks)
+	res.Sum += parRes
+	result = append(result, res)
+
+	if tasks.Child == nil {
+		return result
+	}
+
+	for _, item := range tasks.Child {
+		childRes := seqWork(item, res.Sum)
+		result = append(result, childRes...)
+	}
+	return result
+}
+
+func InitWork(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		panic("Ошибка чтения файла")
+	}
+	tree := readJSON(file)
+	result := seqWork(tree, 0)
+	log.Printf("Result: %+v", result)
 }
