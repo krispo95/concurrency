71540bba3f1723792b263f54b725158eb5568fe8
krisp95 <pogosyan.chris@gmail.com>
2019-04-01 17:55:28 +0300

    DeadLock

--- workers/workers.go
+++ workers/workers.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"time"
 )
 
 type worker struct {
@@ -21,6 +22,9 @@ type TaskResult struct {
 	Sum    int64 `json:"sum"`
 }
 
+var Tasks = make(chan TaskTree, 10)
+var Result = make(chan TaskResult, 10)
+
 func readJSON(r io.Reader) *TaskTree {
 	tree := new(TaskTree)
 	err := json.NewDecoder(r).Decode(tree)
@@ -30,7 +34,85 @@ func readJSON(r io.Reader) *TaskTree {
 	return tree
 }
 
+func doWork(task *TaskTree) (result TaskResult) {
+	for _, item := range task.Task {
+		result.Sum += item
+	}
+	time.Sleep(time.Second)
+	result.TaskId = task.TaskId
+	return result
+}
+func concurrWork(tasks *TaskTree, parRes int64) {
+
+	res := doWork(tasks)
+	res.Sum += parRes
+	Result <- res
+
+	if tasks.Child == nil {
+		return
+	}
+
+	for _, item := range tasks.Child {
+		go concurrWork(item, res.Sum)
+	}
+	return
+}
+func InitWork(path string) {
+	time0 := time.Now()
+	file, err := os.Open(path)
+	if err != nil {
+		panic("Ошибка чтения файла")
+	}
+	tree := readJSON(file)
+	concurrWork(tree, 0)
+	result := []TaskResult{}
+	for item := range Result {
+		result = append(result, item)
+	}
+	log.Printf("result: %+v", result)
+	log.Printf("duration: %+v", time.Now().Sub(time0))
+	log.Printf("Result: %+v", Result)
+}
+
+//-----------------------------------------------
+//func doWork(task *TaskTree) (result TaskResult) {
+//	for _, item := range task.Task {
+//		result.Sum += item
+//	}
+//	time.Sleep(time.Second)
+//	result.TaskId = task.TaskId
+//	return result
+//}
 //
+//func seqWork(tasks *TaskTree, parRes int64) (result []TaskResult) {
+//
+//	res := doWork(tasks)
+//	res.Sum += parRes
+//	result = append(result, res)
+//
+//	if tasks.Child == nil {
+//		return result
+//	}
+//
+//	for _, item := range tasks.Child {
+//		childRes := seqWork(item, res.Sum)
+//		result = append(result, childRes...)
+//	}
+//	return result
+//}
+//
+//func InitWork(path string) {
+//	time0 := time.Now()
+//	file, err := os.Open(path)
+//	if err != nil {
+//		panic("Ошибка чтения файла")
+//	}
+//	tree := readJSON(file)
+//	result := seqWork(tree, 0)
+//	log.Printf("duration: %+v", time.Now().Sub(time0))
+//	log.Printf("Result: %+v", result)
+//}
+//------------------------------------------------
 //func (w *worker) run() {
 //	for {
 //		select {
@@ -41,7 +123,7 @@ func readJSON(r io.Reader) *TaskTree {
 //		}
 //	}
 //}
-
+//
 //func initWorker() {
 //	for i := uint(0); i < 3; i++ {
 //		w := &worker{
@@ -52,7 +134,7 @@ func readJSON(r io.Reader) *TaskTree {
 //		go w.run()
 //	}
 //}
-
+//
 //func (w *worker) doWork(task *TaskTree) (result TaskResult) {
 //	for _, item := range task.Task {
 //		result.Sum += item
@@ -60,38 +142,14 @@ func readJSON(r io.Reader) *TaskTree {
 //	result.TaskId = task.TaskId
 //	return result
 //}
-
-func doWork(task *TaskTree) (result TaskResult) {
-	for _, item := range task.Task {
-		result.Sum += item
-	}
-	result.TaskId = task.TaskId
-	return result
-}
-
-func seqWork(tasks *TaskTree, parRes int64) (result []TaskResult) {
-
-	res := doWork(tasks)
-	res.Sum += parRes
-	result = append(result, res)
-
-	if tasks.Child == nil {
-		return result
-	}
-
-	for _, item := range tasks.Child {
-		childRes := seqWork(item, res.Sum)
-		result = append(result, childRes...)
-	}
-	return result
-}
-
-func InitWork(path string) {
-	file, err := os.Open(path)
-	if err != nil {
-		panic("Ошибка чтения файла")
-	}
-	tree := readJSON(file)
-	result := seqWork(tree, 0)
-	log.Printf("Result: %+v", result)
-}
+//func InitWork(path string) {
+//	time0 := time.Now()
+//	file, err := os.Open(path)
+//	if err != nil {
+//		panic("Ошибка чтения файла")
+//	}
+//	tree := readJSON(file)
+//	result := seqWork(tree, 0)
+//	log.Printf("duration: %+v", time.Now().Sub(time0))
+//	log.Printf("Result: %+v", result)
+//}
